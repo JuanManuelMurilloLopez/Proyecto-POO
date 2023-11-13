7f9de6b0aabb34b42e448fb3412e02a200dc62a8
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2023-11-12 22:51:39 -0600

    Add files via upload

---
+++ objetos.h
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "Jugador.h"
+#include "Equipo.h"
+
+    //Equipo1
+    ptr::Portero portero1Eq1("N1",1,0);
+    dfs::Defensa defensa1Eq1("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq1("N3",3,0,"pos");
+    mdo::Medio medio1Eq1("N4",4,0,"pos");
+    mdo::Medio medio2Eq1("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq1("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq1("N7",7,0,"pos");
+
+    //Equipo2
+    ptr::Portero portero1Eq2("N1",1,0);
+    dfs::Defensa defensa1Eq2("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq2("N3",3,0,"pos");
+    mdo::Medio medio1Eq2("N4",4,0,"pos");
+    mdo::Medio medio2Eq2("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq2("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq2("N7",7,0,"pos");
+
+    //Equipo3
+    ptr::Portero portero1Eq3("N1",1,0);
+    dfs::Defensa defensa1Eq3("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq3("N3",3,0,"pos");
+    mdo::Medio medio1Eq3("N4",4,0,"pos");
+    mdo::Medio medio2Eq3("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq3("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq3("N7",7,0,"pos");
+
+    //Equipo4
+    ptr::Portero portero1Eq4("N1",1,0);
+    dfs::Defensa defensa1Eq4("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq4("N3",3,0,"pos");
+    mdo::Medio medio1Eq4("N4",4,0,"pos");
+    mdo::Medio medio2Eq4("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq4("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq4("N7",7,0,"pos");
+
+    //Equipo5
+    ptr::Portero portero1Eq5("N1",1,0);
+    dfs::Defensa defensa1Eq5("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq5("N3",3,0,"pos");
+    mdo::Medio medio1Eq5("N4",4,0,"pos");
+    mdo::Medio medio2Eq5("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq5("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq5("N7",7,0,"pos");
+
+    //Equipo6
+    ptr::Portero portero1Eq6("N1",1,0);
+    dfs::Defensa defensa1Eq6("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq6("N3",3,0,"pos");
+    mdo::Medio medio1Eq6("N4",4,0,"pos");
+    mdo::Medio medio2Eq6("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq6("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq6("N7",7,0,"pos");
+    
+    //Equipo7
+    ptr::Portero portero1Eq7("N1",1,0);
+    dfs::Defensa defensa1Eq7("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq7("N3",3,0,"pos");
+    mdo::Medio medio1Eq7("N4",4,0,"pos");
+    mdo::Medio medio2Eq7("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq7("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq7("N7",7,0,"pos");
+
+    //Equipo8
+    ptr::Portero portero1Eq8("N1",1,0);
+    dfs::Defensa defensa1Eq8("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq8("N3",3,0,"pos");
+    mdo::Medio medio1Eq8("N4",4,0,"pos");
+    mdo::Medio medio2Eq8("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq8("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq8("N7",7,0,"pos");
+
+    //Equipo9
+    ptr::Portero portero1Eq9("N1",1,0);
+    dfs::Defensa defensa1Eq9("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq9("N3",3,0,"pos");
+    mdo::Medio medio1Eq9("N4",4,0,"pos");
+    mdo::Medio medio2Eq9("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq9("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq9("N7",7,0,"pos");
+
+    //Equipo10
+    ptr::Portero portero1Eq10("N1",1,0);
+    dfs::Defensa defensa1Eq10("N2",2,0,"pos");
+    dfs::Defensa defensa2Eq10("N3",3,0,"pos");
+    mdo::Medio medio1Eq10("N4",4,0,"pos");
+    mdo::Medio medio2Eq10("N5",5,0,"pos");
+    dtr::Delantero delantero1Eq10("N6",6,0,"pos");
+    dtr::Delantero delantero2Eq10("N7",7,0,"pos");
